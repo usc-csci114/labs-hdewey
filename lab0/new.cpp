1cf48c8187a5e676eca903ba86aa53a9bb301b92
henry dewey <hdewey02@gmail.com>
2024-08-28 14:44:21 -0700

    Failed init bc cpp version

--- lab0/new.cpp
+++ lab0/new.cpp
@@ -5,13 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
     
-    vector<int> vec = { 4, 2, 4, 6, 7 };
+    vector<int> vec;
+
+    vec.push_back(4);
+    vec.push_back(3);
+    vec.push_back(6);
+    vec.push_back(7);
+    vec.push_back(4);
 
     vector<int>::iterator it;
 
     for (it = vec.begin(); it != vec.end(); ++it) {
         cout << *it << " ";
     }
+
+    cout << endl;
 
     return 0;
 }
