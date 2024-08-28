31fb80e2db14982eacc7bd45a17b320bb10bc70d
henry dewey <hdewey02@gmail.com>
2024-08-28 09:52:02 -0700

    lab 0 - new.cpp

--- lab0/new.cpp
+++ lab0/new.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-
     
+    vector<int> vec = { 4, 2, 4, 6, 7 };
+
+    vector<int>::iterator it;
+
+    for (it = vec.begin(); it != vec.end(); ++it) {
+        cout << *it << " ";
+    }
 
     return 0;
 }
